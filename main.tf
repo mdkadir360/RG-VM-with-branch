269f6e07b64f1b8926a4b76c7ba8fdce6d8d928b
Abdul Kadir <mdkadir360@gmail.com>
2024-10-01 00:05:27 +0530

    source path edited

--- main.tf
+++ main.tf
@@ -18,39 +18,39 @@ module "con" {
 }
 
 module "vnet" {
-  source     = "github.com/mdkadir360/Terraform_Module_for_Branch//4-Virtual-network"
+  source     = "github.com/mdkadir360/Terraform_Module_for_Branch//4.VNET"
   vnet       = var.vnet
   depends_on = [module.Rg]
 }
 module "subnet" {
-  source     = "github.com/mdkadir360/Terraform_Module_for_Branch//5-Subnet"
+  source     = "github.com/mdkadir360/Terraform_Module_for_Branch//5.SUBNET"
   subnet     = var.subnet
   depends_on = [module.vnet]
 }
 module "pip" {
-  source     = "github.com/mdkadir360/Terraform_Module_for_Branch//6-Publicip"
+  source     = "github.com/mdkadir360/Terraform_Module_for_Branch//6.PUBLIC_IP"
   pip        = var.pip
   depends_on = [module.Rg]
 }
 module "nic" {
-  source     = "github.com/mdkadir360/Terraform_Module_for_Branch//7-Nic"
+  source     = "github.com/mdkadir360/Terraform_Module_for_Branch//7.NIC"
   nic        = var.nic
   depends_on = [module.Rg, module.subnet, module.vnet, module.pip]
 }
 module "nsg" {
-  source     = "github.com/mdkadir360/Terraform_Module_for_Branch//8-NSG"
+  source     = "github.com/mdkadir360/Terraform_Module_for_Branch//8.NSG"
   nsg        = var.nsg
   security   = var.security
   depends_on = [module.Rg]
 }
 
 module "nsgass" {
-  source      = "github.com/mdkadir360/Terraform-env-gitops/tree/main/9-nsg-association"
+  source      = "github.com/mdkadir360/Terraform-env-gitops/tree/main/9.NSG_ASSOCIATION"
   association = var.associationm
   depends_on  = [module.nsg, module.subnet]
 }
 module "vm" {
-  source     = "github.com/mdkadir360/Terraform_Module_for_Branch//10-Linuxvm"
+  source     = "github.com/mdkadir360/Terraform_Module_for_Branch//10.VM"
   vm         = var.vm
   depends_on = [module.Rg, module.nic, module.pip, module.subnet, module.nsgass]
 }
